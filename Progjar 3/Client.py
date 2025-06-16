0c8e2fcbc1be14086b394d9628fae093c684f11a
adeliarahmaa <162128135+adeliarahmaa@users.noreply.github.com>
2025-06-16 16:53:37 +0700

    Update Client.py

--- Progjar 3/Client.py
+++ Progjar 3/Client.py
@@ -1,1 +1,35 @@
+import socket
+import logging
+
+logging.basicConfig(level=logging.WARNING, format='[%(asctime)s] [CLIENT] %(message)s')
+
+def kirim_data():
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_address = ('localhost', 45000)
+    logging.warning(f"Membuka koneksi ke {server_address}")
+    sock.connect(server_address)
+
+    try:
+        while True:
+            pesan = input("Ketik perintah (TIME / QUIT): ").strip().upper()
+            if pesan not in ['TIME', 'QUIT']:
+                print("Hanya boleh memasukkan 'TIME' atau 'QUIT'")
+                continue
+
+            kirim = pesan + '\r\n'  # Sesuai spesifikasi tugas (char 13 dan 10)
+            sock.sendall(kirim.encode())
+            logging.warning(f"Mengirim: {pesan}")
+
+            data = sock.recv(64).decode().strip()
+            logging.warning(f"Balasan: {data}")
+
+            if pesan == 'QUIT':
+                break
+
+    finally:
+        logging.warning("Menutup koneksi")
+        sock.close()
+
+if __name__ == '__main__':
+    kirim_data()
 
